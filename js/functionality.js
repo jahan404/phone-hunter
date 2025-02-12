859e0e114fceec5874fda61697d3ebc7ea0082a0
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-02-12 12:59:56 +0600

    all functionality done

--- js/functionality.js
+++ js/functionality.js
@@ -1,26 +1,32 @@
-async function loadAfterThreeSecondsAndApi(status,brandname){
+async function loadAfterThreeSecondsAndApi(status,brandName){
     // console.log('after 3 seconds')
     document.getElementById('loading-spinner').classList.add('hidden')
 
 
-    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandname?brandname:'iphone'}`)
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName:'iphone'}`)
     const data = await response.json()
     
-    if(status){
-        displayFromApi(data.data)
-    }
-    else{
-        displayFromApi(data.data.slice(0,6))
-    }
+
+    displayFromApi(data.data,status)
+    
 }
 
 
 //load all phones from api-->> make phone cards
-const displayFromApi =(data) =>{
-    // console.log(data)
-    const cardContainer = document.getElementById('card-container')
+const displayFromApi =(data,status) =>{
 
-    data.forEach((phone)=>{
+    let phones;
+    if(status){
+        phones =(data)
+    }
+    else{
+        phones = (data.slice(0,6))
+    }
+    // console.log(phones)
+
+    const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML=``
+    phones.forEach((phone)=>{
     
     const {brand,phone_name,slug,image} = phone
     const div = document.createElement('div')
@@ -35,7 +41,7 @@ const displayFromApi =(data) =>{
   <div class="card-body items-center text-center">
     <h2 class="card-title">${phone_name}</h2>
     <div class="card-actions">
-      <button class="btn btn-primary mt-2">Details</button>
+      <button onclick="showModalDetails('${slug}')" class="btn btn-primary mt-2">Details</button>
     </div>
   </div>
 </div>   
@@ -45,10 +51,47 @@ const displayFromApi =(data) =>{
 }
 
 
+//modal detail + modal creation
+const showModalDetails = async(slugs)=>{
+
+    const modalId = document.getElementById('my_modal')
+    modalId.showModal();
+    // console.log(modalId)
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/phone/${slugs}`)
+    const dataApi = await response.json()
+    console.log(dataApi.data)
+
+
+    const {image,brand,others,mainFeatures,releaseDate,name,slug} = dataApi.data
+    
+
+    const modalContent = document.getElementById('modal-content')
+    modalContent.innerHTML=``
+
+        const div = document.createElement('div')  
+        div.innerHTML=`
+        <div>
+        <img src="${image}"/>
+        </div>
+        <h1 class="text-xl font-semibold">${name}</h1>
+        <p class="">${brand}</p>
+        <p class="">${mainFeatures.sensors}</p>
+        <p class="">${others?.WLAN}</p>
+        <p class="">${others?.Bluetooth}</p>
+        <p class="">${others?.GPS}</p>
+        <p class="">${others?.USB}</p>
+        `
+        modalContent.appendChild(div)
+    
+}
+
 
 //show all button
 const ShowAll = () => {
-    loadAfterThreeSecondsAndApi(true,'iphone')
+    const searchText = document.getElementById('user-input').value
+    loadAfterThreeSecondsAndApi(true,searchText)
+    document.getElementById('show-all-button').classList.add('hidden')
 }
 
 
@@ -64,7 +107,9 @@ function setTimeOutFunc(){
     setTimeout(()=>{
         loadAfterThreeSecondsAndApi(false,searchText)
     },3000)
+
+    document.getElementById('show-all-button').classList.remove('hidden')
 }
 
 
-// loadAfterThreeSecondsAndApi('','')
+// loadAfterThreeSecondsAndApi(false,'iphone')
